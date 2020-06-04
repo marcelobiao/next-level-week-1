b3cd1d5f3339c5850416990c173b977a8f3a9b28
Marcelo Biao <marcelobiao2@gmail.com>
2020-06-04 12:54:03 -0300

    Ajustes nas rotas da aplicação

--- src/routes.ts
+++ src/routes.ts
@@ -1,6 +1,26 @@
-import express from 'express';
+import express, { response } from 'express';
+import knex from './database/connection';
+
+import PointsController from './controllers/PointsController';
 
 const routes = express.Router();
+
+routes.get('/items', async (req, res) =>{
+  const items = await knex('items').select('*');
+ 
+  const serializedItems = items.map(item => {
+    return {
+      id: item.id,
+      name: item.title,
+      image_url: `http://localhost:3333/uploads/${item.image}`
+    }
+  })
+
+  return res.json(serializedItems);
+})
+
+routes.post('/points', PointsController.create);
+
 
 routes.get('/', (req,res) => {
   return res.json('Hello!'); 
